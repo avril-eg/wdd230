6a33954c5a59eef0d299058c33f70c0486eeb04c
avril-eg <131208921+avril-eg@users.noreply.github.com>
2023-05-23 12:21:44 -0700

    Delete wdd230 directory

--- wdd230/README.md
+++
@@ -1,2 +0,0 @@
-# wdd230
-Repository for the WDD230 Spring Semester at BYU-Idaho.
--- wdd230/scripts/dates.js
+++
@@ -1,5 +0,0 @@
-const copyrightyear = document.getElementById("copyrightyear");
-copyrightyear.textContent = new Date().getFullYear();
-
-const lastModified = document.getElementById("lastModified");
-lastModified.textContent = `Last Updated: ${document.lastModified}`;
