021284d5ac0d385ec0860126fd3ca17a1b521de7
avril-eg <131208921+avril-eg@users.noreply.github.com>
2023-05-24 15:11:51 -0700

    Add files via upload

---
+++ scripts/currentDate.js
@@ -0,0 +1,9 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+// long, medium, short options ... try them
+datefield.innerHTML = `<div>${fulldate}</div>`;
