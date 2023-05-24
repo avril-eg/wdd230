6c3ca3e8a8334f3125e7fdfeb95f520d9aacfeaf
avril-eg <131208921+avril-eg@users.noreply.github.com>
2023-05-24 15:13:53 -0700

    Add files via upload

--- chamber/scripts/ham.js
+++ chamber/scripts/ham.js
@@ -1,7 +1,8 @@
 // Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.menu')
+const hambutton = document.querySelector('.hamBtn');
 
 // Add a simple arrow function that listens for the <li> hamburger button click event.
 // When clicked, the <ul class="navigation">'s class list toggle'
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
---
+++ chamber/scripts/meet.js
@@ -0,0 +1,4 @@
+// Join us message based on day of the week
+if (d.getDay() == 1 || d.getDay() == 2) {
+    document.getElementsByClassName("meet").style.display = "block";
+  }
