a2122cf5a93159a47d152b2fed2211f1f2227ce2
avril-eg <131208921+avril-eg@users.noreply.github.com>
2023-06-20 21:16:46 -0700

    Add files via upload

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -23,12 +23,6 @@ function displayProphets(prophet) {
   p2.textContent = `Place of Birth: ${prophet.birthplace}`;
 
   portrait.setAttribute("src", prophet.imageurl);
-  portrait.setAttribute(
-    "alt",
-    `Portait of ${prophet.name} ${prophet.lastname} - ${returnOrdinal(
-      prophet.order
-    )} Latter-day President`
-  );
   portrait.setAttribute("loading", "lazy");
 
   card.appendChild(h2);
@@ -39,22 +33,4 @@ function displayProphets(prophet) {
   document.querySelector("div.cards").appendChild(card);
 }
 
-function returnOrdinal(num) {
-  let stringNum = num.toString();
-  let endsWith1 = stringNum.endsWith("1");
-  let endsWith2 = stringNum.endsWith("2");
-  let endsWith3 = stringNum.endsWith("3");
-
-  if (endsWith1 && (num < 10 || num > 20)) {
-    return `${num}st`;
-  }
-  if (endsWith2 && (num < 10 || num > 20)) {
-    return `${num}nd`;
-  }
-  if (endsWith3 && (num < 10 || num > 20)) {
-    return `${num}rd`;
-  }
-
-  return `${num}th`;
-}
 
