dcdd0014f113fe9a8f39b0d2069df9f59ceaf144
avril-eg <131208921+avril-eg@users.noreply.github.com>
2023-06-24 23:25:46 -0700

    Add files via upload

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,14 +1,59 @@
-// Calculate Wind Chill
-const temperatureF = Math.round((document.getElementById("temperature").textContent * (9/5)) + 32);
-const speedmph = Math.round(document.getElementById("windSpeed").textContent / 1.609);
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temp');
+const currentWindSpeed = document.querySelector('#windspeedresult');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Hermosillo,MX&units=imperial&appid=9f3292324049b798caa4a57c365df48d';
 
-if (temperatureF <= 50 && speedmph > 3.0) {
-    const chill = Math.round(35.74 + 0.6215 * temperatureF - 35.75 * speedmph ** 0.16 + 0.4275 * temperatureF * speedmph ** 0.16);
-    document.getElementById("windChill").textContent = `${Math.round(chill * 1.609)} \u00B0C`;
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+    function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}&deg;F</strong>`;
+    currentWindSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)} Mph</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
+
+
+
+function windchill () {
+
+    const temp = document.getElementById('temp').innerHTML;
+    const windspeed = document.getElementById('windspeedresult').innerHTML; 
+    const windchill = document.getElementById('windchillresult');
+   
+    if ( temp <= 50 && windspeed > 3) { 
+
+        let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp * Math.pow(windspeed, 0.16);
+        wc = Math.floor(wc);
+
+        windchill.innerHTML = wc + "°F";
+    }
+    else{
+
+        windchill.innerHTML = "N/A"; 
+    }
+
+    return windchill;
 }
 
-
-
-
-
-
+windchill();
