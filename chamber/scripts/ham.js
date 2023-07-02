543cfa6968f497b2fa7f4a61d60d57894aeaef89
avril-eg <131208921+avril-eg@users.noreply.github.com>
2023-07-01 22:41:00 -0700

    Add files via upload

--- chamber/scripts/ham.js
+++ chamber/scripts/ham.js
@@ -3,11 +3,8 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hambBtn").classList.toggle("open");
 }
-
 const x = document.getElementById("hambBtn");
 x.onclick = toggleMenu;
-
-
 
 
 
