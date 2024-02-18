79d387025e4ed24aba063d974b90a56307370e45
avril-eg <eavril.eg@gmail.com>
2024-02-17 23:39:13 -0700

    windchill

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,14 @@
+let windchill = function(temperature, windspeed) {
+    if (temperature <= 50 && windspeed > 3) {
+  
+        return Math.round((
+            35.74 + 
+            (0.6215 * temperature) - 
+            (35.75 * Math.pow(windspeed, 0.16)) + 
+            (0.4275 * temperature * Math.pow(windspeed, 0.16))
+        ));
+    }
+    else {
+        return '0';
+    }
+  }
