96307c99e07cc843bd2361da23eecdf30dca1c6d
avril-eg <eavril.eg@gmail.com>
2024-02-22 17:23:51 -0700

    chamber

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,14 +1,21 @@
-let windchill = function(temperature, windspeed) {
-    if (temperature <= 50 && windspeed > 3) {
+function calculateWindchill(temperature, windspeed) {
+    return 35.74 + (0.6215 * temperature) - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
+}
   
-        return Math.round((
-            35.74 + 
-            (0.6215 * temperature) - 
-            (35.75 * Math.pow(windspeed, 0.16)) + 
-            (0.4275 * temperature * Math.pow(windspeed, 0.16))
-        ));
+
+function windchill() {
+    let temperature = parseFloat(document.getElementById('temperature').textContent);
+    let windspeed = parseFloat(document.getElementById('windspeed').textContent);
+
+
+    if (temperature <= 50 && windspeed > 3) {
+        let windchill = calculateWindchill(temperature, windspeed);
+      
+        document.getElementById('windchill').textContent = windchill.toFixed(0);
+    } else {
+   
+        document.getElementById('windchill').textContent = 'N/A';
     }
-    else {
-        return '0';
-    }
+
+    
   }
