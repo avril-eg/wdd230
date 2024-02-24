295e368249e999dbbb66c3fd13ee3a7ff472079a
avril-eg <eavril.eg@gmail.com>
2024-02-23 17:34:29 -0700

    discovery

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -2,3 +2,15 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
+const modeButton = document.querySelector("#checkbox");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+  if (modeButton.checked) {
+    main.style.background = "#000";
+    main.style.color = "#fff";
+  } else {
+    main.style.background = "#eee";
+    main.style.color = "#000";
+  }
+});
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,21 +1,13 @@
-function calculateWindchill(temperature, windspeed) {
-    return 35.74 + (0.6215 * temperature) - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
-}
-  
-
-function windchill() {
-    let temperature = parseFloat(document.getElementById('temperature').textContent);
-    let windspeed = parseFloat(document.getElementById('windspeed').textContent);
+document.addEventListener("DOMContentLoaded", function() {
+  const temperature = document.querySelector("#temp"); 
+  const windspeed = document.querySelector("#speed"); 
 
 
-    if (temperature <= 50 && windspeed > 3) {
-        let windchill = calculateWindchill(temperature, windspeed);
-      
-        document.getElementById('windchill').textContent = windchill.toFixed(0);
-    } else {
-   
-        document.getElementById('windchill').textContent = 'N/A';
-    }
-
-    
+  if (temperature <= 50 && windspeed > 3.0) {
+      const windchill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
+      windchillValue.textContent = `Wind Chill: ${windchill.toFixed(2)}°F`;
+  } else {
+      windchillValue.textContent = 'Wind Chill: N/A';
   }
+});
+
