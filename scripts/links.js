83f55b600b918a9dbb04f9b10da86e98156fc887
avril-eg <eavril.eg@gmail.com>
2024-03-05 16:49:56 -0700

    links-json

--- scripts/links.js
+++ scripts/links.js
@@ -1,49 +1,70 @@
 const baseURL = "https://avril-eg.github.io/wdd230/";
 const linksURL = "https://avril-eg.github.io/wdd230/data/links.json";
+const linksList = document.querySelector("#linklist");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
+
+getLinks();
+
+function displayLinks(lessons) {
+    lessons.forEach(lesson => {
+        const li = document.createElement("li");
+        li.innerHTML = `${lessons.lesson}:`
+        lesson.links.forEach(link => {
+            li.innerHTML += ` <a href="${link.url}">${link.title}</a> |`
+        })
+        li.innerHTML = li.innerHTML.slice(0, -1);
+        
+        linksList.appendChild(li);
+    });
+}
+
+
+
+
+  
+
+  
+    
+  
+
   }
   
   getLinks();
 
-
-  fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-
-  .then(function (jsonObject) {
-
-    //console.table(jsonObject); for temporary checking for valid response and data parsing
-    
-    const prophets = jsonObject['prophets'];
-   
-   // for (let i = 0; i < prophets.length; i++ ) { 
-      
+  const displayProphets = (prophets) => {
     prophets.forEach(prophet => {
-        const card = document.createElement('section');
-        const h2 = document.createElement('h2');
-        const birthDate = document.createElement('p');
-        const birthPlace = document.createElement('p');
-        const image = document.createElement('img');
-        const fullname = `${prophet.name} ${prophet.lastname}`;
+        // Create elements to add to the div.cards element
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2");
+        let birthInfo = document.createElement("p");
+        let portrait = document.createElement("img");
+        
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthInfo.innerHTML = `Date of Birth: ${prophet.birthdate} <br>Place of Birth: ${prophet.birthplace}`;
+        
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("heigth", "440");
 
-        h2.textContent = fullname;
-        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-        birthPlace.textContent =`Place of Birth: ${prophet.birthplace}`;
-        image.setAttribute('src', prophet.imageurl);
-        image.setAttribute('alt', `${fullname} - ${prophet.order}`);
+        card.appendChild(fullName);
+        card.appendChild(birthInfo);
+        card.appendChild(portrait);
 
-     
-        card.appendChild(h2);
-        card.appendChild(birthDate);
-        card.appendChild(birthPlace);
-        card.appendChild(image);
+        cards.appendChild(card);
+    });
+}
 
-      document.querySelector('#cards').appendChild(card);
-            
-    })
-  });
+
+
+
+  
+
+  
+    
+  
