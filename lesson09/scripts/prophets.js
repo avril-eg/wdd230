a6230d64d9d4444dbca31e65f7e3b3ebf5ac0b4f
avril-eg <eavril.eg@gmail.com>
2024-03-04 21:38:39 -0700

    prophets

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,39 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (jsonObject) {
+
+    //console.table(jsonObject); for temporary checking for valid response and data parsing
+    
+    const prophets = jsonObject['prophets'];
+   
+   // for (let i = 0; i < prophets.length; i++ ) { 
+      
+    prophets.forEach(prophet => {
+        const card = document.createElement('section');
+        const h2 = document.createElement('h2');
+        const birthDate = document.createElement('p');
+        const birthPlace = document.createElement('p');
+        const image = document.createElement('img');
+        const fullname = `${prophet.name} ${prophet.lastname}`;
+
+        h2.textContent = fullname;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent =`Place of Birth: ${prophet.birthplace}`;
+        image.setAttribute('src', prophet.imageurl);
+        image.setAttribute('alt', `${fullname} - ${prophet.order}`);
+
+     
+        card.appendChild(h2);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(image);
+
+      document.querySelector('#cards').appendChild(card);
+            
+    })
+  });
