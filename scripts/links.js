d9da7b90c38248f784c9d249c0784ce7e7d49a58
avril-eg <eavril.eg@gmail.com>
2024-03-07 23:18:55 -0700

    links

--- scripts/links.js
+++ scripts/links.js
@@ -1,31 +1,41 @@
 const baseURL = "https://avril-eg.github.io/wdd230/";
 const linksURL = "https://avril-eg.github.io/wdd230/data/links.json";
-const linksList = document.querySelector("#linklist");
+//const linksList = document.querySelector("#linklist");
+
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     displayLinks(data.lessons);
+}
 
 getLinks();
 
-function displayLinks(lessons){
-    
+const displayLinks = (weeks) => {
+    // console.log(weeks);
 
-    lessons.forEach(lesson => {
-        let li = document.createElement("li");
-        li.textContent = `${lessons.lesson}`;
-        lesson.links.forEach((link, i) =>{
-            let a = document.createElement('a');
+    weeks.forEach (week=>{
+        // console.log(week);
+        const listItem = document.createElement("li");
+        const lessonWeek = document.createTextNode(`${week.lesson}: `);
+
+        listItem.appendChild(lessonWeek);
+
+        week.links.forEach((link) => {
+            // console.log(link);
+            const a = document.createElement("a");
             a.setAttribute("href", link.url);
-            if (i === lesson.links.length - 1){
-                a.textContent = `${link.title}`;
-            }else{
-                a.textContent = `${link.title} | `;
+            a.textContent = link.title;
+
+            listItem.appendChild(a);
+
+            if (week.links.length > 1 && week.links.indexOf(link) < week.links.length - 1) {
+                listItem.innerHTML += " | ";
             }
-            li.appendChild(a); 
-        })  
-        // add li element to the ul element
-        linksList.appendChild(li);      
+        });
+        // console.log(listItem);
+
+        const list = document.querySelector("#linklist");
+        list.appendChild(listItem);
     });
-}}
+};
