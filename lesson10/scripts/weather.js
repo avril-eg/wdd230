12aab3edcfb63c0d670436e67ae9d2d439ac3a7f
avril-eg <eavril.eg@gmail.com>
2024-03-11 21:21:50 -0700

    weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,10 +1,35 @@
 // Select HTML element in the document
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document-querySelector('#weather-icon');
-const captionDesc =document.querySelector('figcaption');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap-org/data/2.5/_';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=a682a4d8efb27d168b538433ff7508e5&units=imperial';
 
 async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            //displayWeatherResults(data);
+        } else ;{
+            throw Error(await response.text())
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherEvent.icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt',desc);
+    captionDesc.textContent = `${desc}`;
+
+    currentTemp.appendChild(currentTemp);
     
+    displayResults()
 }
