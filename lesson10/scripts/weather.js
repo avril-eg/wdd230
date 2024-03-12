7d8acc3c463c01cde4cbe85aa32858d4db5a3c9f
avril-eg <eavril.eg@gmail.com>
2024-03-11 19:47:15 -0700

    weather

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,10 @@
+// Select HTML element in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document-querySelector('#weather-icon');
+const captionDesc =document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap-org/data/2.5/_';
+
+async function apiFetch() {
+    
+}
