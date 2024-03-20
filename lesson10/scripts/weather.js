b4b35132f3a4c1dab7404e10d0b2a213ed93bf34
avril-eg <eavril.eg@gmail.com>
2024-03-19 17:06:38 -0700

    updates

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,7 +1,3 @@
-// Select HTML element in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=a682a4d8efb27d168b538433ff7508e5&units=imperial';
 
@@ -10,26 +6,44 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            //console.log(data);
-            //displayWeatherResults(data);
-        } else ;{
-            throw Error(await response.text())
-        } 
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw new Error(await response.text());
+        }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/wn/${weatherEvent.icon}.png`;
-    let desc = data.weather[0].icon;
-    weatherIcon.setAttribute('src',iconsrc);
-    weatherIcon.setAttribute('alt',desc);
-    captionDesc.textContent = `${desc}`;
+    const currentTemp = document.querySelector("#current-temp");
+    const weatherIcon = document.querySelector("#weather-icon");
+    const captionDesc = document.querySelector("#weather-description");
 
-    currentTemp.appendChild(currentTemp);
+    // Format temperature to show zero decimal points
+    const formattedTemp = data.main.temp.toFixed(0);
+    // Display current temperature
+    currentTemp.innerHTML = `${formattedTemp} &deg;F`;
 
-    displayResults()
+    // Display weather icon and description
+    data.weather.forEach((weatherEvent) => {
+        const iconsrc = `https://openweathermap.org/img/wn/${weatherEvent.icon}@2x.png`;
+        let desc = weatherEvent.description;
+        desc = capitalizeSting(desc);
+        weatherIcon.setAttribute("src", iconsrc);
+        weatherIcon.setAttribute("alt", desc);
+        captionDesc.innerHTML = `${desc}`;
+    });
+}
+
+function capitalizeSting(myString) {
+    const words = myString.split(" ");
+
+    return words
+        .map((word) => {
+            return word[0].toUpperCase() + word.substring(1);
+        })
+        .join(" ");
 }
