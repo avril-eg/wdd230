bfaf6e887f074a083aa2c9364dfa86ed9298bd5e
avril-eg <eavril.eg@gmail.com>
2024-03-19 23:50:23 -0700

    updates

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,16 +1,21 @@
-    const banner = document.getElementById("banner");
+    const banner = document.querySelector(".banner");
     const weekDay = new Date().getDay();
 
-    if (weekDay == 1 || weekDay == 2 || weekDay == 3){
-        const button = document.createElement('button');
-        button.textContent = '❌';
-        banner.appendChild(button);
-        button.addEventListener('click', () =>{
+if (weekDay == 1 || weekDay == 2 || weekDay == 3){
+    const button = document.createElement('button');
+    button.textContent = 'X';
+    const invitation = document.createElement('div');
+    const title = document.createElement('h2');
+    title.textContent = `Join us for the Chamber of Commerce meet and greet on Wednesday at 7:00 p.m.!`;
+    banner.appendChild(button);
+    invitation.appendChild(title);
+    banner.appendChild(invitation);
+    button.addEventListener('click', () =>{
         banner.classList.add('closed-banner')
-        })
-    }
-    else{
-        banner.setAttribute('style', 'display: none');
-    }
+    })
+}
+else{
+    banner.setAttribute('style', 'display: none');
+}
 
-    
+
