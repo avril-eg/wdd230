4075e1f22d08f99d95f1706f3640186f4c415226
avril-eg <eavril.eg@gmail.com>
2024-03-25 19:07:30 -0700

    final-project

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,11 +1,23 @@
-const visitsDisplay = document.querySelector(".visits");
+const visitsDisplay = document.querySelector("#last");
+let lastVisit = window.localStorage.getItem("lastVisit-ws");
 
-let numVisits = Number(localStorage.getItem("numVisits-key")) || 0;
 
-if (numVisits !== 0) {
-    visitsDisplay.innerHTML = `<strong>${numVisits}</strong>`;
+
+if (lastVisit) {
+lastVisit = Number(lastVisit); // Convert lastVisit to a number
+const millisecondsPerDay = 24 * 60 * 60 * 1000;
+const daysSinceLastVisit = Math.floor((currentDate - lastVisit) / millisecondsPerDay);
+
+if (daysSinceLastVisit <= 0) {
+    visitsDisplay.textContent = 'Back so soon! Awesome!';
+} else if (daysSinceLastVisit === 1) {
+    visitsDisplay.textContent = 'You last visited 1 day ago.';
+} else {
+    visitsDisplay.textContent = `You last visited ${daysSinceLastVisit} days ago.`;
+}
+} else {
+    // Handle the first visit case
+    visitsDisplay.textContent = 'Welcome! Let us know if you have any questions.';
 }
 
-numVisits++;
-
-localStorage.setItem("numVisits-key", numVisits);
+localStorage.setItem('lastVisit-ws', currentDate); 
